d9d751deab353066a7e30bc723e65147dbf6a5a5
Nazar <youtopak1@gmail.com>
2023-03-28 11:12:06 +0300

    add folder dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -17,17 +19,50 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/scss/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/scss/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('font', function() {
+    return gulp.src('src/font/**/*')
+    .pipe(gulp.dest('dist/font'));
+});
+
+gulp.task('icons', function() {
+    return gulp.src('src/icons/**/*')
+    .pipe(gulp.dest('dist/icons'));
+});
+
+gulp.task('dataBase', function() {
+    return gulp.src('src/dataBase/**/*')
+    .pipe(gulp.dest('dist/dataBase'));
+});
+
+gulp.task('serverPhp', function() {
+    return gulp.src('src/server/**/*')
+    .pipe(gulp.dest('dist/server'));
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'font', 'icons', 'dataBase', 'serverPhp', 'img'));
